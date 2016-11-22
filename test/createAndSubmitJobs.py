e793fc15f5f77b069e3c985d7905c5d2dc000b05
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2016-11-22 19:14:59 +0100

    updated job submission script

--- test/createAndSubmitJobs.py
+++ test/createAndSubmitJobs.py
@@ -23,35 +23,28 @@ def process_input_dir(inputDir, match, filelist):
 
     path = inputDir;
     name = ''
-    jobdict = {}
+    joblist = []
 
     for filename in filenamelist:
         if( not re.search('.root$', filename) ):
             continue
         if ( match!=None and not re.search(match, filename) ):
             continue
-        m1 = re.search('_\d+_\d+_\w+.root', filename)
+        m1 = re.search('_\d+.root', filename)
         if name=='':
-            name = re.split('_\d+_\d+_\w+.root', filename)[0]
-        jobstring = filename[m1.start():].lstrip('_').replace('.root','').split('_')
-        job = int(jobstring[0])
-        if job not in jobdict.keys():
-            jobdict[job] = []
-            jobdict[job].append([int(jobstring[1])])
-            jobdict[job].append([jobstring[2]])
-        else:
-            jobdict[job][0].append(int(jobstring[1]))
-            jobdict[job][1].append(jobstring[2])
+            name = re.split('_\d+.root', filename)[0]
+        jobstring = filename[m1.start():].lstrip('_').replace('.root','')
+        job = int(jobstring)
+        if job not in joblist:
+            joblist.append(job)
 
-    jobs = jobdict.keys()
-    if( len(jobs)==0 ):
+    if( len(joblist)==0 ):
         print 'No matching .root files found'
         sys.exit()
 
-    jobs.sort()
-    for job in jobs:
-        maxsub = max(jobdict[job][0])
-        filename = (path+name+'_%i_%i_%s.root')%(job, maxsub, jobdict[job][1][jobdict[job][0].index(maxsub)])
+    joblist.sort()
+    for job in joblist:
+        filename = (path+name+'_%i.root')%job
         filelist.append(filename)
 
     return
@@ -64,23 +57,9 @@ Output = DUMMY_OUTPUTDIR/logs/condor_$(Cluster)_$(Process).stdout
 Error = DUMMY_OUTPUTDIR/logs/condor_$(Cluster)_$(Process).stderr
 Log = DUMMY_OUTPUTDIR/logs/condor_$(Cluster)_$(Process).log
 Arguments = $(Process) DUMMY_OUTPUTDIR DUMMY_PARAMS
-+RULength="short"
 Queue DUMMY_NJOBS
 """
 
-jdl_template_fnal = """universe = vanilla
-Should_Transfer_Files = YES
-WhenToTransferOutput = ON_EXIT
-Notification = never
-x509userproxy = $ENV(X509_USER_PROXY)
-Executable = DUMMY_OUTPUTDIR/CMSSW.sh
-Output = condor_$(Cluster)_$(Process).stdout
-Error = condor_$(Cluster)_$(Process).stderr
-Log = condor_$(Cluster)_$(Process).log
-Arguments = $(Process) DUMMY_OUTPUTDIR DUMMY_PARAMS
-+LENGTH="SHORT"
-Queue DUMMY_NJOBS
-"""
 
 bash_template = """#!/bin/bash
 
@@ -92,8 +71,8 @@ START_TIME=`/bin/date`
 echo "Started at $START_TIME"
 echo ""
 
-export SCRAM_ARCH=slc5_amd64_gcc462
-source /cms/base/cmssoft/cmsset_default.sh
+export SCRAM_ARCH=MY_SCRAM_ARCH
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd $OUTPUTDIR
 eval `scramv1 runtime -sh`
 
@@ -118,48 +97,9 @@ echo "Finished at $END_TIME"
 exit $exitcode
 """
 
-bash_template_fnal = """#!/bin/bash
-
-CONDOR_PROCESS=$1
-OUTPUTDIR=$2
-PARAMS=$3
-
-START_TIME=`/bin/date`
-echo "Started at $START_TIME"
-echo ""
-
-export SCRAM_ARCH=slc6_amd64_gcc472
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd $OUTPUTDIR
-eval `scramv1 runtime -sh`
-
-# change to Condor scratch directory
-cd ${_CONDOR_SCRATCH_DIR}
-
-# define output log file
-LOG="CMSSW_Job_${CONDOR_PROCESS}.log"
-# separate individual input parameters
-PARAMS_MOD=${PARAMS//,/ }
-
-# rewrite cfg file with input parameters applied
-python ${OUTPUTDIR}/CMSSW_cfg.py outFilename=CMSSW_Job_${CONDOR_PROCESS}.root \\
-useExternalInput=True externalInput=${OUTPUTDIR}/input/files_${CONDOR_PROCESS}.txt \\
-dumpPythonCfg=CMSSW_Job_${CONDOR_PROCESS}_cfg.py \\
-$PARAMS_MOD
-
-echo "Running CMSSW job $CONDOR_PROCESS using the following input parameters: $PARAMS_MOD"
-cmsRun -p CMSSW_Job_${CONDOR_PROCESS}_cfg.py > $LOG 2>&1
-exitcode=$?
-
-echo ""
-END_TIME=`/bin/date`
-echo "Finished at $END_TIME"
-exit $exitcode
-"""
-
 
 usage = """Usage: %prog [-m MATCH] -i INPUTDIR -c CMSSW_CFG -o OUTPUTDIR -n NJOBS [-p PARAMS -f 1.0 --create-only]\n
-Example: ./createAndSubmitJobs.py -i /cms/ferencek/store/ferencek/QCD_Pt-120to170_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v3_BTagAnalyzer_V02-05-03/ -c tagvarextractor_cfg.py -o output_dir -n 60 -p maxEvents=-1,reportEvery=1000,wantSummary=True"""
+Example: ./createAndSubmitJobs.py -i /STORE/se/cms/store/user/mrogulji/BoostedDoubleBTagger/QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8/crab_BDBTagger_ak8_puppi_20161122_153535/161122_143549/0000/ -c tagvarextractor_cfg.py -o output_dir -n 50 -p maxEvents=-1,reportEvery=100,wantSummary=True"""
 
 
 def main():
@@ -174,7 +114,6 @@ def main():
     parser.add_option('-p', '--params', metavar='PARAMS', action='store', dest='params', default='', help='Comma separated list of job input parameters (This parameter is optional)')
     parser.add_option('-f', '--fraction', metavar='FRACTION', action='store', dest='fraction', default='1.0', help='Fraction of files to be processed. Default value is 1 (This parameter is optional)')
     parser.add_option('--create-only', action="store_true", dest="create_only", default=False, help="Create the necessary configuration files but skip the job submission (This parameter is optional)")
-    parser.add_option('--fnal', action="store_true", dest="fnal", default=False, help="This switch is mandatory if submitting jobs at FNAL")
 
     (options, args) = parser.parse_args(args=None)
 
@@ -237,8 +176,6 @@ def main():
     # create jdl file
     jdl_file = open(os.path.join(outputmain,'CMSSW.jdl'),'w')
     jdl_content = jdl_template
-    if options.fnal:
-        jdl_content = jdl_template_fnal
     jdl_content = re.sub('DUMMY_OUTPUTDIR',outputmain,jdl_content)
     jdl_content = re.sub('DUMMY_NJOBS',str(ijobmax),jdl_content)
     jdl_content = re.sub('DUMMY_PARAMS',params,jdl_content)
@@ -247,8 +184,7 @@ def main():
     # create Bash script
     bash_script = open(os.path.join(outputmain,'CMSSW.sh'),'w')
     bash_content = bash_template
-    if options.fnal:
-        bash_content = bash_template_fnal
+    bash_content = re.sub('MY_SCRAM_ARCH',os.environ['SCRAM_ARCH'],bash_content)
     bash_script.write(bash_content)
     bash_script.close()
     os.system('chmod +x '+ os.path.join(outputmain,'CMSSW.sh'))
@@ -256,20 +192,18 @@ def main():
     ifile = 0
     for ijob in range(ijobmax):
 	# prepare the list of input files
-	inputfiles = (filelist[ifile].replace('/eos/uscms/store/user/','root://cmsxrootd-site.fnal.gov//store/user/') if options.fnal else filelist[ifile])
+	inputfiles = 'file:' + filelist[ifile]
 	for i in range(filesperjob-1):
 	    if ifile>(numfiles-2):
 	        break
 	    ifile = ifile + 1
-	    inputfiles += ( '\n' + (filelist[ifile].replace('/eos/uscms/store/user/','root://cmsxrootd-site.fnal.gov//store/user/') if options.fnal else filelist[ifile]) )
+	    inputfiles += ( '\n' + 'file:' + filelist[ifile] )
 	ifile = ifile + 1
 	
 	# create a text file containing input files
 	open(os.path.join(outputmain,'input','files_' + str(ijob) + '.txt'),'w').write(inputfiles)
 	
     if not options.create_only:
-      if options.fnal:
-        os.chdir(os.path.join(outputmain,'output'))
       # submit Condor jobs
       os.system('condor_submit ' + os.path.join(outputmain,'CMSSW.jdl'))
 
